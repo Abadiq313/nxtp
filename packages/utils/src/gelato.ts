b3c3b9e4ecb7d536f22494cc353158c626b55259
Sanchay Mittal <sanchaymittal@gmail.com>
2021-11-24 19:01:17 +0530

    new: all functions for Wrapper Router

--- packages/utils/src/gelato.ts
+++ packages/utils/src/gelato.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Interface } from "ethers/lib/utils";
 
-import { FulfillParams } from "./transactionManager";
+import { FulfillParams, PrepareParams, CancelParams } from "./transactionManager";
 
 const ACCESS_TOKEN = "4942987b-af28-4ab7-bf75-4bd383e82f80";
 
@@ -25,7 +25,7 @@ const endpoints = {
   [CHAIN_ID.AVALANCHE]: "https://relay.avalanche.fra.gelato.digital/relay",
 };
 
-const sendFulfill = async (
+const gelatoSend = async (
   chainId: number,
   dest: string,
   data: string,
@@ -46,21 +46,73 @@ const sendFulfill = async (
   return output;
 };
 
-const gelatoFulfill = async (
-  chainId: number,
-  address: string,
-  abi: Interface,
-  fulfillArgs: FulfillParams,
-): Promise<any> => {
-  const args = { ...fulfillArgs, encodedMeta: "0x" };
-  const data = abi.encodeFunctionData("fulfill", [args]);
-  const token = fulfillArgs.txData.receivingAssetId;
-  const ret = await sendFulfill(chainId, address, data, token, fulfillArgs.relayerFee);
-  return ret;
-};
-
-const isChainSupportedByGelato = (chainId: number): boolean => {
+export const isChainSupportedByGelato = (chainId: number): boolean => {
   return Object.values(CHAIN_ID).indexOf(chainId) !== -1;
 };
 
-export { gelatoFulfill, isChainSupportedByGelato };
+/// NXTP-ROUTER-CONTRACTS
+
+export const gelatoAddLiquidity = async (
+  chainId: number,
+  address: string,
+  abi: Interface,
+  params: { amount: string; assetId: string; relayerFee: string },
+): Promise<any> => {
+  const args = { ...params };
+  const data = abi.encodeFunctionData("addLiquidity", [args]);
+  const token = args.assetId;
+  const ret = await gelatoSend(chainId, address, data, token, args.relayerFee);
+  return ret;
+};
+
+export const gelatoRemoveLiquidity = async (
+  chainId: number,
+  address: string,
+  abi: Interface,
+  _args: { amount: string; assetId: string; relayerFee: string },
+): Promise<any> => {
+  const args = { ..._args };
+  const data = abi.encodeFunctionData("removeLiquidity", [args]);
+  const token = args.assetId;
+  const ret = await gelatoSend(chainId, address, data, token, args.relayerFee);
+  return ret;
+};
+
+export const gelatoPrepare = async (
+  chainId: number,
+  address: string,
+  abi: Interface,
+  _args: { params: PrepareParams; relayerFee: string },
+): Promise<any> => {
+  const args = { ..._args };
+  const data = abi.encodeFunctionData("prepare", [args]);
+  const token = args.params.txData.receivingAssetId;
+  const ret = await gelatoSend(chainId, address, data, token, args.relayerFee);
+  return ret;
+};
+
+export const gelatoFulfill = async (
+  chainId: number,
+  address: string,
+  abi: Interface,
+  _args: FulfillParams,
+): Promise<any> => {
+  const args = { ..._args, encodedMeta: "0x" };
+  const data = abi.encodeFunctionData("fulfill", [args]);
+  const token = args.txData.receivingAssetId;
+  const ret = await gelatoSend(chainId, address, data, token, args.relayerFee);
+  return ret;
+};
+
+export const gelatoCancel = async (
+  chainId: number,
+  address: string,
+  abi: Interface,
+  _args: { params: CancelParams; relayerFee: string },
+): Promise<any> => {
+  const args = { ..._args };
+  const data = abi.encodeFunctionData("cancel", [args]);
+  const token = args.params.txData.receivingAssetId;
+  const ret = await gelatoSend(chainId, address, data, token, args.relayerFee);
+  return ret;
+};
